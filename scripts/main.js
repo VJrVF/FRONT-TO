f4890d74c0a38b993f44796e74fd56b868ebdbe9
Victor <vvillanuevaf@uni.pe>
2022-01-30 14:18:14 -0500

    cambio getdata

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ const items = document.querySelector('.items');
 function getData() {
     fetch(`${serverUrl}${itemsPath}`)
        .then((res) => res.json())
-       .then((data) => printData(data));
+       //.then((data) => printData(data));
 }
 
 function printData(data) {
